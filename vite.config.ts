26b76c848f593284f080312cc6dd99c294eebfc8
Ronnie04NYC <162711934+Ronnie04NYC@users.noreply.github.com>
2025-11-29 13:34:17 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     define: {
-      // Safely replace process.env.API_KEY with the string value
+      // Safely replace process.env.API_KEY with the actual value during build
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
     },
     build: {
+      // Increases the warning limit to avoid the "chunk size" warning
       chunkSizeWarningLimit: 1600,
-    }
+    },
   };
 });
